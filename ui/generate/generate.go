8240e41d626637875d78ecebb8dd7cae88121513
Valentin Rogister <valentin.rogister@gmail.com>
2024-07-04 14:45:41 +0200

    parallelismed generation

--- ui/generate/generate.go
+++ ui/generate/generate.go
@@ -2,6 +2,7 @@ package generate
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io/fs"
 	"log"
@@ -14,8 +15,64 @@ import (
 
 var uiPath = filepath.Join(utils.GetCurrentDirectory(), "..")
 
+type Cmd struct {
+	cmd    *exec.Cmd
+	stdout bytes.Buffer
+	stderr bytes.Buffer
+}
+
+func NewCmd(cmd *exec.Cmd) *Cmd {
+	c := &Cmd{cmd: cmd}
+	c.cmd.Stdout = &c.stdout
+	c.cmd.Stderr = &c.stderr
+	return c
+}
+
+func (c *Cmd) Start() error {
+	return c.cmd.Start()
+}
+
+func (c *Cmd) Wait() string {
+	err := c.cmd.Wait()
+	fmt.Println(c.cmd.Args)
+	if err != nil {
+		log.Fatal(c.stderr.String())
+	}
+	return c.stdout.String()
+}
+
 func Generate() {
 	idCount := 1
+
+	cmds := []*Cmd{}
+
+	cmds = append(cmds, NewCmd(exec.Command("postcss", filepath.Join(uiPath, "style", "global", "style.css"))))
+
+	err := filepath.Walk(filepath.Join(uiPath, "templates"), func(path string, info fs.FileInfo, err error) error {
+		if !info.IsDir() && info.Name() == "style.css" {
+			idString := fmt.Sprintf("id-%d", idCount)
+			idCount += 1
+
+			f, _ := os.Create(filepath.Join(filepath.Dir(path), "generated_id.go"))
+			defer f.Close()
+			_, _ = f.WriteString(fmt.Sprintf("package %s\n\n", filepath.Base(filepath.Dir(path))))
+			_, _ = f.WriteString(fmt.Sprintf("const id = \"%s\"\n", idString))
+
+			cmd := exec.Command("postcss", path)
+			cmd.Env = append(os.Environ(), fmt.Sprintf("PREFIX=%s", idString))
+			fmt.Println(idString)
+			cmds = append(cmds, NewCmd(cmd))
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, cmd := range cmds {
+		cmd.Start()
+	}
+
 	distPath := filepath.Join(uiPath, "dist")
 	_ = os.RemoveAll(distPath)
 	_ = os.Mkdir(distPath, os.ModePerm)
@@ -26,40 +83,8 @@ func Generate() {
 	defer file.Close()
 	writer := bufio.NewWriter(file)
 
-	cmd := exec.Command("npx", "postcss", filepath.Join(uiPath, "style", "global", "style.css"))
-	cmd.Dir = uiPath
-	output, err := cmd.CombinedOutput()
-	fmt.Println(string(output))
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	writer.Write(output)
-
-	err = filepath.Walk(filepath.Join(uiPath, "templates"), func(path string, info fs.FileInfo, err error) error {
-		if !info.IsDir() && info.Name() == "style.css" {
-			idString := fmt.Sprintf("id-%d", idCount)
-			idCount += 1
-
-			f, _ := os.Create(filepath.Join(filepath.Dir(path), "generated_id.go"))
-			defer f.Close()
-			_, _ = f.WriteString(fmt.Sprintf("package %s\n\n", filepath.Base(filepath.Dir(path))))
-			_, _ = f.WriteString(fmt.Sprintf("const id = \"%s\"\n", idString))
-
-			cmd := exec.Command("npx", "postcss", path)
-			cmd.Env = append(os.Environ(), fmt.Sprintf("PREFIX=%s", idString))
-			fmt.Println(idString)
-			cmd.Dir = uiPath
-			output, err := cmd.CombinedOutput()
-			fmt.Println(string(output))
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-			writer.Write(output)
-		}
-		return nil
-	})
-	if err != nil {
-		log.Fatal(err)
+	for _, cmd := range cmds {
+		writer.WriteString(cmd.Wait())
 	}
 
 	err = writer.Flush()
